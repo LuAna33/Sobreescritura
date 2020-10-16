37bbe833a3d06603225fc5915ccd6666b810b0cf
Luisa Garin <luy22@live.com.ar>
2020-10-16 18:57:05 -0300

    Actualizacion

---
+++ src/test/TestSobreescritura.java
@@ -0,0 +1,15 @@
+package test;
+
+//import domain.Gerente;
+
+import domain.Empleado;
+
+public class TestSobreescritura {
+    public static void main(String[] args) {
+        //Gerente gerente1 = new Gerente("Juan Acosta", 250.5,"Exportaciones");
+        //System.out.println("gerente1 = " + gerente1.obtenerDetalles());
+
+        Empleado empleado1 = new Empleado("Luis Perez", 500);
+
+    }
+}
